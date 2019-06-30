026ff87c476bceeb4f66b4e8c78781abde43aace
Veronica Enamorado <veronica.enamorado@gmail.com>
2019-07-01 01:27:06 +0200

    Build version for deployment

---
+++ docs/precache-manifest.2cf6aa7ecdbbd832efea3f6008ea0ea9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a77ce1f6b4b6e9cdbb9fd35441fd094",
+    "url": "./index.html"
+  },
+  {
+    "revision": "823b95da1487b6dbeafc",
+    "url": "./static/css/main.0c0de8b2.chunk.css"
+  },
+  {
+    "revision": "6044ae0d18e8ada2a7cc",
+    "url": "./static/js/2.d9abeecc.chunk.js"
+  },
+  {
+    "revision": "823b95da1487b6dbeafc",
+    "url": "./static/js/main.2ff43f71.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
